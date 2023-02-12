8bc630f4f10bf2977bfdbcbb376293da4e2da316
Irina04 <120029999+Irina04@users.noreply.github.com>
2023-02-12 17:27:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,32 +3,29 @@ function addText() {
     let listNode = document.getElementById('dictionar');
     let liNode= document.createElement("li");
     let textNode = document.createTextNode(text);
-
     liNode.appendChild(textNode);
-    listNode.appendChild(liNode);
-    
-    let clearInput = document.getElementById('addword');
-    if (clearInput.value != "") {
-        clearInput.value = "";
-    }
+    listNode.appendChild(liNode); 
+    clearInput('addword');
 }
 function searchForWord() {
     let textForsearch = document.getElementById('searchforword').value;
     let elements = document.getElementsByTagName('li');
     let found = 0;
     for (let elem of elements) {
-       let text = elem.innerHTML;
+        let text = elem.innerHTML;
         if (text == textForsearch){
             alert('The word was found!');
             found = 1;
         }
     }
     if (found == 0) {
-         alert('The word was not found!');
+        alert('The word was not found!');
     }
-
-    let clearInput = document.getElementById('searchforword');
-    if (clearInput.value != "") {
-        clearInput.value = "";
+    clearInput('searchforword');
+}
+function clearInput(x) {
+    let clear = document.getElementById(x);
+    if (clear.value != "") {
+        clear.value = "";
     }
 }
